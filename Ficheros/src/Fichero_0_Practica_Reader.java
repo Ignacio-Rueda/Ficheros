3a4bcb9dc5fcacdf3f0a569d3e459105704bd2bd
Ignacio-Rueda <nachord@outlook.com>
2024-03-12 20:28:35 +0100

    Pruebas lectura ficheros, por línea o carácter a carácter.

---
+++ Ficheros/src/Fichero_0_Practica_Reader.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Fichero_0_Practica_Reader {
+
+	public static void main(String args[]) {
+		
+		//Lectura fichero.
+		FileReader fr = null;
+		BufferedReader br = null;
+	
+		try {
+			fr = new FileReader("c:/ficheros/datos.txt");
+			br = new BufferedReader(fr);
+			
+			String linea = br.readLine();
+			while(linea!=null) {
+				System.out.println(linea);
+				linea = br.readLine();
+			}
+			
+		}catch(FileNotFoundException ex) {
+			System.out.println(ex.getMessage());
+		}catch(IOException ex) {
+			System.out.println(ex.getMessage());
+		}finally {
+			try {
+				if(fr != null) {
+					fr.close();
+				}
+			}catch(IOException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+		System.out.println("--------------------------------------");
+		System.out.println("AHORA VAMOS A LEER CARÁCTER A CARÁCTER");
+		System.out.println("--------------------------------------");
+		
+		try {
+			fr = new FileReader("c:/ficheros/caracter.txt");
+			br = new BufferedReader(fr);
+			int character = br.read();
+			while(character != -1) {
+				System.out.println((char)character);
+				character = br.read();
+			}
+		}catch(FileNotFoundException ex) {
+			System.out.println(ex.getMessage());
+		}catch(IOException ex) {
+			System.out.println(ex.getMessage());
+		}finally {
+			try {
+				if(fr != null) {
+					fr.close();
+				}
+			}catch(IOException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+		
+	}
+}
